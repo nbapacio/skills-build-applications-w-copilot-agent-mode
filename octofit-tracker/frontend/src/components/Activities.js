44e14f8b6323a7ad3695a41f8ab5ca4e1301c571
nbapacio <nino.brian.a.pacio@accenture.com>
2025-11-11 10:33:56 +0000

    Commit

---
+++ octofit-tracker/frontend/src/components/Activities.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+const Activities = () => {
+  const [activities, setActivities] = useState([]);
+  const endpoint = `https://${process.env.REACT_APP_CODESPACE_NAME}-8000.app.github.dev/api/activities/`;
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(data => {
+        console.log('Activities API endpoint:', endpoint);
+        console.log('Fetched activities:', data);
+        setActivities(data.results ? data.results : data);
+      });
+  }, [endpoint]);
+
+  return (
+    <div className="card mt-4">
+      <div className="card-body">
+        <h2 className="card-title mb-4">Activities</h2>
+        <div className="table-responsive">
+          <table className="table table-striped table-bordered">
+            <thead className="table-dark">
+              <tr>
+                <th>User</th>
+                <th>Type</th>
+                <th>Duration (min)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {activities.map((activity, idx) => (
+                <tr key={idx}>
+                  <td>{activity.user}</td>
+                  <td>{activity.type}</td>
+                  <td>{activity.duration}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Activities;
